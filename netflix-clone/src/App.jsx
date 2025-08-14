754acff881262f3dce4eb567494c63b53f91aecd
Abdisa.dev <meetabdisa@gmail.com>
2025-08-14 11:18:40 +0300

    axios and request

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      hello world
-      
-    </>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
